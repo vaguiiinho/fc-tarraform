39cd0b6261abcb9bb7f303f5a14b2f7ede1a41e5
vaguiiinho <avanlendario@gmail.com>
2023-12-19 21:30:32 -0300

    add subnet

--- vpc.tf
+++ vpc.tf
@@ -4,3 +4,13 @@ resource "aws_vpc" "new-vpc" {
     Name = "fullcycle-vpc"
   }
 }
+
+data "aws_availability_zones" "available" {}
+resource "aws_subnet" "new-subnet-1" {
+  availability_zone = "us-east-1a"
+  vpc_id     = aws_vpc.new-vpc.id
+  cidr_block = "10.0.1.0/24"
+    tags = {
+    Name = "fullcycle-subnet-1"
+  }
+}
