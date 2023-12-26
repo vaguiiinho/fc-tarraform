0c1301046892ed073621cafd2644fef5f43057e1
vaguiiinho <avanlendario@gmail.com>
2023-12-26 15:01:44 -0300

    finish resource

--- nodes.tf
+++ nodes.tf
@@ -31,21 +31,21 @@ resource "aws_iam_role_policy_attachment" "node-AmazonEC2ContainerRegistryReadOn
   role       = aws_iam_role.node.name
 }
 
-# resource "aws_eks_node_group" "node-1" {
-#   cluster_name = aws_eks_cluster.cluster.name
-#   node_group_name = "node-1"
-#   node_role_arn = aws_iam_role.node.arn
-#   subnet_ids = aws_subnet.subnets[*].id
-#   #instance_types = ["t3.micro"]
-#   scaling_config {
-#     desired_size = var.desired_size
-#     max_size = var.max_size
-#     min_size = var.min_size
-#   }
+resource "aws_eks_node_group" "node-1" {
+  cluster_name = aws_eks_cluster.cluster.name
+  node_group_name = "node-1"
+  node_role_arn = aws_iam_role.node.arn
+  subnet_ids = aws_subnet.subnets[*].id
+  instance_types = ["t3.micro"]
+  scaling_config {
+    desired_size = var.desired_size
+    max_size = var.max_size
+    min_size = var.min_size
+  }
   
-#   depends_on = [
-#     aws_iam_role_policy_attachment.node-AmazonEKSWorkerNodePolicy,
-#     aws_iam_role_policy_attachment.node-AmazonEKS_CNI_Policy,
-#     aws_iam_role_policy_attachment.node-AmazonEC2ContainerRegistryReadOnly,
-#   ]
-# }
+  depends_on = [
+    aws_iam_role_policy_attachment.node-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.node-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.node-AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
